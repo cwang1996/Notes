55cacf82b14cff51960af0ba72e3f388cb625554
Calvin Wang <calvinwang@yahoo.com>
2021-08-02 16:03:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,12 @@ const add_btn = document.querySelector('.add');
 const delete_btn = document.querySelector('.delete');
 const date_span = document.querySelector('.date');
 const textArea = document.querySelector('.note');
-
-// window.onload() = function(){
-//     createElement();
-// }
-
-// function createElement(){
-//     let createNote = document.createElement('div');
-//     createNote.appendChild();
-// }
+const save_btn = document.querySelector('.save');
 
 function addNote() {
     let today = new Date();
     let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     add_btn.addEventListener('click', function(){
-        // let div = document.createElement('div');
-        // div.classList.add('notes', 'notes-header');
-        // container.appendChild(div);
         let notesClone = notes.cloneNode(true);
         container.appendChild(notesClone);
         date_span.innerHTML = date;
@@ -29,9 +18,38 @@ function addNote() {
 }
 
 function removeNote(event) {
-    // event.target.parentNode.parentNode.parentNode.style.display = 'none';
     event.target.parentNode.parentNode.parentNode.remove();
     console.log(event.currentTarget);
 }
 
+function saveNoteDate(){
+    save_btn.addEventListener('click', () => {
+        if(typeof(Storage) !== 'undefined') {
+            let date_input = date_span.value;
+            localStorage.setItem('date', date_input);
+            console.log('Title Saved');
+        } else {
+            alert('Your browser does not support web storage');
+        }
+    })
+}
+
+function saveNoteMessage(){
+    save_btn.addEventListener('click', () => {
+        if(typeof(Storage) !== 'undefined'){
+            let messageInput = textArea.value;
+            console.log(messageInput);
+            localStorage.setItem('note', messageInput);
+            console.log('Message Saved');
+        } else {
+            alert('Your browser does not support web storage');
+        }
+    })
+}
+
+date_span.value = localStorage.getItem('date');
+textArea.value = localStorage.getItem('note');
+
 addNote();
+saveNoteDate();
+saveNoteMessage();
